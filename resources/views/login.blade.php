78116bbb81c123ec8b59d6430c6897373ddba449
Shakya Rujen <b7134@oic.jp>
2018-07-02 00:25:37 +0900

    buttons の　追加

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,29 +5,36 @@
  <link href="{{('css/style.css') }}" rel="stylesheet" />
     </head>
     <body>
-        <form>Email:<br>
+        <form id="login">
+            <h1>LOGIN</h1>
+            
+            Email:<br>
         <input type="email" name ="SignUpEmail"><br>
         Password:<br>
-        <input type="password" name ="Password">
+        <input type="password" name ="Password"><br><br>
+        <input type="button" value="Login">
+        </form>
+        <div id="signup">
         <h1>Create a New Account</h1>
         <h2>It's free and always will be.<br>
              <input type="text" name ="First Name" value="First Name"> <input type="text" name ="Last Name" value="Last Name">
         <br><input type="email" name ="LoginEmail" value="Email"><br>
         <input type="password" name="password" value="Password"><br>
+
+        
         <p>Birthday</p>
 <li>
 <select><option>Day</option></select>
 <select><option>Month</option></select>
 <select><option>Year</option></select><br>
-<a href="">Why do I need to provide my date of birth?</a>
-
-<li><input type="radio">Female <input type="radio">Male</li>
-<li id="terms">By clicking Create an account, you agree to our <a href="">Terms</a> and that <br>you have read our <a href="">Data Policy</a>, including our <a href="">Cookie Use</a>.</li>
-<li><input type="submit" value="Create an account"></li>
-<li id="create_page"><a href="">Create a Page</a> for a celebrity, band or business.</li>
 </li>
-            
-    </form>
+<br>
+<input type="checkbox">Male
+<input type="checkbox">Female
+<br><br>
+<input type="button" value="SignUp">
+
+</div>
     </body>
 
 
