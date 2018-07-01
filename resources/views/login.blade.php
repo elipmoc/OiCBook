4798a4d90eb8884e965f9801e50f9920569c8a7f
Shakya Rujen <b7134@oic.jp>
2018-07-01 23:29:25 +0900

    Skeleton Practice

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,1 +1,34 @@
-hello
+<!doctype html>
+<html>
+    <head>
+    <title>OiCBook</title>
+ <link href="{{('css/style.css') }}" rel="stylesheet" />
+    </head>
+    <body>
+        <form>Email:<br>
+        <input type="email" name ="SignUpEmail"><br>
+        Password:<br>
+        <input type="password" name ="Password">
+        <h1>Create a New Account</h1>
+        <h2>It's free and always will be.<br>
+             <input type="text" name ="First Name" value="First Name"> <input type="text" name ="Last Name" value="Last Name">
+        <br><input type="email" name ="LoginEmail" value="Email"><br>
+        <input type="password" name="password" value="Password"><br>
+        <p>Birthday</p>
+<li>
+<select><option>Day</option></select>
+<select><option>Month</option></select>
+<select><option>Year</option></select><br>
+<a href="">Why do I need to provide my date of birth?</a>
+
+<li><input type="radio">Female <input type="radio">Male</li>
+<li id="terms">By clicking Create an account, you agree to our <a href="">Terms</a> and that <br>you have read our <a href="">Data Policy</a>, including our <a href="">Cookie Use</a>.</li>
+<li><input type="submit" value="Create an account"></li>
+<li id="create_page"><a href="">Create a Page</a> for a celebrity, band or business.</li>
+</li>
+            
+    </form>
+    </body>
+
+
+    </html>
